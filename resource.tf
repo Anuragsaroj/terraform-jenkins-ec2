a1fec2e8937b7bd41198bcb75f210066b6ffde4c
Anuragsaroj <104360708+Anuragsaroj@users.noreply.github.com>
2025-03-27 22:26:46 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "public_ec2" {
-  ami                    = "ami-076c6dbba59aa92e6"
+  ami                    = "ami-0e35ddab05955cf57"
   instance_type           = "t2.micro"
   key_name               = "jenkins27"
   availability_zone      = "ap-south-1a"
